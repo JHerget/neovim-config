ed161184fd1f5c6d6729fb17b0b8f75cfa8a6c27
Jordan Herget <lethalbacon16@gmail.com>
2025-07-30 12:02:41 -0600

    updated keymaps

--- core/keymaps.lua
+++ core/keymaps.lua
@@ -13,6 +13,7 @@ vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally 
 vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
 vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+vim.keymap.set("n", "<leader>sf", ":resize | vertical resize<CR>") -- full screen the current window
 
 vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
@@ -24,13 +25,17 @@ vim.keymap.set("n", "<leader><Down>", "<C-w>j") -- move down a window
 vim.keymap.set("n", "<leader><Up>", "<C-w>k") -- move up a window
 vim.keymap.set("n", "<leader><Right>", "<C-w>l") -- move right a window
 
--- vim.keymap.set("n", "<leader>w<Left>", ":resize -1<CR>") -- -1 window width
--- vim.keymap.set("n", "<leader>w<Down>", ":vertical resize -1<CR>") -- -1 window height
--- vim.keymap.set("n", "<leader>w<Up>", ":vertical resize +1<CR>") -- +1 window height
--- vim.keymap.set("n", "<leader>w<Right>", ":resize +1<CR>") -- +1 window width
-
 vim.keymap.set("n", "<C-x>", ":bd<CR>") -- close current buffer
 
 -- LSP keymaps
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
 vim.keymap.set('n', '<leader>sd', vim.diagnostic.open_float)
+
+vim.api.nvim_create_augroup("AutoFormatting", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+    group = "AutoFormatting",
+    pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.json" },
+    callback = function()
+        vim.lsp.buf.format()
+    end,
+})
